98029eac4f2b49646df9a3d44661702b1eaaddae
9900-W18C-Almond <z5603812@ad.unsw.edu.au>
2026-03-14 21:32:34 +1100

    Xcode Frontend Update

---
+++ mac-app/Whispr/Whispr/Models.swift
@@ -0,0 +1,36 @@
+import AppKit
+
+enum AppStatus: String, CaseIterable {
+    case idle
+    case listening
+    case processing
+    case error
+
+    var menuBarIcon: NSImage {
+        let imageName: String
+        switch self {
+        case .idle:
+            imageName = "mic.slash"
+        case .listening:
+            imageName = "mic.fill"
+        case .processing:
+            imageName = "waveform.circle.fill"
+        case .error:
+            imageName = "mic.badge.xmark"
+        }
+
+        let image = NSImage(systemSymbolName: imageName, accessibilityDescription: rawValue)
+            ?? NSImage(systemSymbolName: "mic", accessibilityDescription: "default")!
+        image.isTemplate = true
+        return image
+    }
+}
+
+enum TranscriptionMode: String {
+    case clean
+    case formal
+    case chat
+    case code
+    case academic
+    case generic
+}
