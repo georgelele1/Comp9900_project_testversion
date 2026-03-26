02643302ab5127b3f03e2d71280398cae0a270fe
yanbo wang <z5603812@ad.unsw.edu.au>
2026-03-26 18:56:51 +1100

    for google calendar api

---
+++ backend/get_token.py
@@ -0,0 +1,11 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+flow = InstalledAppFlow.from_client_secrets_file(
+    "credentials.json",
+    scopes=["https://www.googleapis.com/auth/calendar.readonly"]
+)
+creds = flow.run_local_server(port=0)
+
+# Save token for future use
+with open("token.json", "w") as f:
+    f.write(creds.to_json())
